83da3ed12cc9995d6751d69b4e3fb0a57cee6116
Yzuaks <124011820+Yzuaks@users.noreply.github.com>
2023-02-01 11:13:11 +0700

    feat:authentication

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthenticationController extends Controller
+{
+    public function login (Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required',
+            'device_name' => 'required|string',
+        ]);
+
+        $user = User::whereEmail($request->email)->first();
+
+        //jika user tidak ada atau password tidak sesuai
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            
+            //kita ngasi pesan error 
+            throw ValidationException::withMessages([
+
+                //dengan key 'email. dan pesan 'Email atau Password Salah!!'
+                'email' => 'Email atau Password Salah!!',
+            ]);
+        }
+
+        return [
+        //jika benar atau berhasil kita mau ngasi access_token/kunci
+        //kita kasi perintah bikin token dengan parameter device_name untuk membedakan token mana untuk device yang mana
+        //kita mau nampilin token yang bisa kita baca
+        'access_token' => $user->createToken($request->device_name)->plainTextToken,
+        
+        //dan kembalikan usernya
+        'user' => '$user',
+        ];
+    }
+
+    public function logout ()
+    {
+        
+    }
+
+    public function register (Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|confirmed',
+        ]);
+
+        return User::create([
+            'name'  => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+    }
+
+    public function profile (Request $request)
+    {
+        return $request->user();
+    }
+}
---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthenticationTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_user_can_login() 
+    {
+        $user = User::factory()->createOne();
+        $data = [
+            'email' => $user->email,
+            'password' => 'password',
+            'device_name' => 'testing',
+        ];
+
+        $this->postJson(route('auth.login'), $data)
+        //assertOK untuk mengecek berhasil atau tidak
+        ->assertOK()
+        //untuk memastikan data jika benar benar OK
+        ->assertJsonStructure(['access_token', 'user']);
+    }
+
+    public function test_user_can_register() 
+    {
+        $data = [
+            'name' => 'Tester',
+            'email' => 'tester@gmail.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'device_name' => 'testing'
+        ]; 
+
+        $this->postJson(route('auth.register'), $data)
+        ->assertCreated()
+        ->assertJsonFragment(['email' => $data['email']]);
+    }
+
+    public function test_user_can_see_their_profile() 
+    {
+        $user = User::factory()->createOne();
+
+        Sanctum::actingAs($user);
+
+        $this->getJson(route('auth.profile'))
+        ->assertOk()
+        ->assertJsonFragment(['email' => $user->email]);
+    }
+
+    public function test_user_cannot_see_their_profile_when_unauthenticated() 
+    {
+        $this->getJson(route('auth.profile'))->assertUnauthorized();
+    }
+
+    public function test_user_can_logout() 
+    {
+        $user =  User::factory()->createOne();
+
+        sanctum::actingAS($user);
+
+        $this->getjson(route('auth.logout'))->assertOK();
+    }
+}
