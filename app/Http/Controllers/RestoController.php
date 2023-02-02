52a45be7b1865afe60de137c927cf9156b492f44
Yzuaks <124011820+Yzuaks@users.noreply.github.com>
2023-02-02 10:08:46 +0700

    feat:resto

--- app/Http/Controllers/RestoController.php
+++ app/Http/Controllers/RestoController.php
@@ -4,11 +4,52 @@ namespace App\Http\Controllers;
 
 use App\Models\Resto;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreRestoRequest;
+use App\Http\Requests\UpdateRestoRequest;
+
+
 
 class RestoController extends Controller
 {
     public function index()
     {
+
         return Resto::all();
+
     }
+
+    public function store(StoreRestoRequest $request)
+    {
+
+       return Resto::create(
+        $request->validated()
+       );
+
+    }
+
+    public function show(Resto $resto)
+    {
+
+       return $resto;
+
+    }
+
+    public function update(UpdateRestoRequest $request, Resto $resto)
+    {
+
+        $resto->update($request->validated());
+
+        return $resto->refresh();
+
+    }
+
+    public function destroy(Resto $resto)
+    {
+
+       $resto->delete();
+
+       return $resto;
+
+    }
+
 }
--- tests/Feature/RestoTest.php
+++ tests/Feature/RestoTest.php
@@ -14,30 +14,55 @@ class RestoTest extends TestCase
 
  public function test_user_can_list_all_resto()
  {
+   
     $count = 5;
     $data = Resto::factory()->count(5)->create();
 
     $this->getJson(route('restos.index'))
     ->assertOK()
     ->assertJsonCount($count);
+
  }
 
  public function test_user_can_create_resto()
  {
-    //
+
+    $data = Resto::factory()->makeOne()->toArray();
+
+    $this->postJson(route('restos.store'), $data)->assertCreated();
+ 
  }
 
  public function test_user_can_show_resto()
  {
-    //
+
+    $data = Resto::factory()->createOne();
+
+    $this->getJson(route('restos.show', $data))
+      ->assertOk()
+      ->assertJsonStructure(['name', 'description', 'address']);
+
  }
  public function test_user_can_edit_resto()
  {
-    //
-}
+
+    $updateData = Resto::factory()->makeOne()->toArray();
+    $data = Resto::factory()->createOne();
+
+    $this->patchJson(route('restos.update', $data), $updateData)
+      ->assertOk()
+      ->assertJsonStructure(['name', 'description', 'address']);
+
+ }
  public function test_user_can_delete_resto()
  {
-    //
+
+    $data = Resto::factory()->createOne();
+
+    $this->deleteJson(route('restos.destroy', $data))
+    ->assertOk()
+    ->assertJsonStructure(['name', 'description', 'address']);
+
  }
 
 }
